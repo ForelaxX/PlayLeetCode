379aafd4c2428b69c8eef72a93bf0489890fbd9a
Forelax <helloworldmkv@163.com>
2017-10-29 21:09:15 +0800

    Longest Substring Without Repeating Characters:third try

--- Longest Substring Without Repeating Characters.playground/Contents.swift
+++ Longest Substring Without Repeating Characters.playground/Contents.swift
@@ -1,29 +1,19 @@
 class Solution {
     func lengthOfLongestSubstring(_ s: String) -> Int {
-        // 第一个条件是，字符串内部不能有重复的
-        // 所以基本思路就是，遍历 string，遍历的过程中不断把字符放到一个 hash 里头
-        // 在接下来的遍历过程中如果发现有重复的，就清空 hash，继续遍历
         if s.count == 0 {
             return 0
         }
         var lengthOfLongestSubstring = 0
-        var firstExistIndexArray : [Int] = []
         var characterPreviousIndexDict : [Character : Int] = [:]
-        var previousIndex = 0
         var startIndex = 0
         for (index, character) in s.enumerated() {
-            if let characterIndex = characterPreviousIndexDict[character] {
-                previousIndex = characterIndex
-            } else {
-                previousIndex = index
-            }
-            characterPreviousIndexDict[character] = index
-            if index != previousIndex && startIndex <= previousIndex {
+            if let previousIndex = characterPreviousIndexDict[character], startIndex <= previousIndex {
                 startIndex = previousIndex + 1
             } else {
                 lengthOfLongestSubstring = max(lengthOfLongestSubstring, index - startIndex + 1)
             }
-            //print("\(s[s.index(s.startIndex, offsetBy: index)]):\(index):\(previouseIndex), start index: (startIndex), length: \(lengthOfLongestSubstring)")
+//            print("\(s[s.index(s.startIndex, offsetBy: index)]):\(index):\(characterPreviousIndexDict[character]), start index: (startIndex), length: \(lengthOfLongestSubstring)")
+            characterPreviousIndexDict[character] = index
         }
         return lengthOfLongestSubstring
     }
