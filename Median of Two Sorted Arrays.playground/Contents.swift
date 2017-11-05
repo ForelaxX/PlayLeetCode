0442ad58f90f27a4c9e80c528102b7ebd96afbd8
Forelax <helloworldmkv@163.com>
2017-11-05 21:48:34 +0800

    Median of Two Sorted Arrays first learn

--- Median of Two Sorted Arrays.playground/Contents.swift
+++ Median of Two Sorted Arrays.playground/Contents.swift
@@ -1,40 +1,60 @@
-//: Playground - noun: a place where people can play
 
 class Solution {
     func findMedianSortedArrays(_ nums1: [Int], _ nums2: [Int]) -> Double {
-        var newNums : Array<Int> = []
-        var localNums1 = Array(nums1)
-        var localNums2 = Array(nums2)
-        if nums1.count == 0 && nums2.count == 0 {
-            return 0
-        }
-        repeat {
-            if let nums1FirstEle = localNums1.first, let nums2FirstEle = localNums2.first {
-                if nums1FirstEle < nums2FirstEle {
-                    newNums.append(nums1FirstEle)
-                    localNums1.remove(at: 0)
-                } else {
-                    newNums.append(nums2FirstEle)
-                    localNums2.remove(at: 0)
-                }
-            } else if let nums1FirstEle = localNums1.first {
-                newNums.append(nums1FirstEle)
-                localNums1.remove(at: 0)
-            } else if let nums2FirstEle = localNums2.first {
-                newNums.append(nums2FirstEle)
-                localNums2.remove(at: 0)
-            } else {
-                print("wrong")
-            }
-        } while localNums1.count > 0 || localNums2.count > 0
-        
-        var rtValue : Double = 0
-        if newNums.count % 2 == 0 {
-            rtValue =  Double((newNums[newNums.count / 2 - 1] + newNums[newNums.count / 2])) / 2
+        var localNums1 : Array<Int>, localNums2 : Array<Int>
+        if nums1.count > nums2.count {
+            localNums1 = Array(nums2)
+            localNums2 = Array(nums1)
         } else {
-            rtValue = Double(newNums[newNums.count / 2])
+            localNums1 = Array(nums1)
+            localNums2 = Array(nums2)
         }
-        return rtValue
+        
+        var mini = 0
+        var maxi = localNums1.count
+        // +1 是为了，当  count 之和是奇数的时候，
+        let halfLength = (localNums1.count + localNums2.count + 1) / 2
+        // means the count of elements belongs to the left part
+        var i : Int
+        // means the count of elements belongs to the left part
+        var j : Int
+        while mini <= maxi {
+            i = (mini + maxi) / 2
+            j = halfLength - i
+            
+            if i > 0 && localNums1[i - 1] > localNums2[j] {
+                maxi = i - 1
+            } else if i < localNums1.count && localNums2[j - 1] > localNums1[i] {
+                mini = i + 1
+            } else {
+                // perfect
+                var maxLeft : Int
+                if i == 0 {
+                    maxLeft = localNums2[j - 1]
+                } else if j == 0 {
+                    maxLeft = localNums1[i - 1]
+                } else {
+                    maxLeft = max(localNums1[i - 1], localNums2[j - 1])
+                }
+                
+                if (localNums1.count + localNums2.count) % 2 == 1 {
+                    return Double(maxLeft)
+                }
+                
+                var minRight : Int
+                if i == localNums1.count  {
+                    minRight = localNums2[j]
+                } else if j == localNums2.count {
+                    minRight = localNums1[i]
+                } else {
+                    minRight = min(localNums1[i], localNums2[j])
+                }
+                print(maxLeft)
+                print(minRight)
+                return Double(maxLeft + minRight) / 2.0
+            }
+        }
+        return 0
     }
 }
 
@@ -45,3 +65,4 @@ let nums2 = [2]
 
 s.findMedianSortedArrays(nums1, nums2) == 2
 s.findMedianSortedArrays([1, 2], [3, 4]) == 2.5
+s.findMedianSortedArrays([4,5], [1,2,3]) == 3
